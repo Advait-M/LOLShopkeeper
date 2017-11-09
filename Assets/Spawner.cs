cda067202068ba1924eadf462d81f23e98800ad7
James Glenn <jglenn1@gmail.com>
2017-11-09 09:48:19 -0800

    Initial stab at spawn-on-pickup

---
+++ Assets/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Spawner : MonoBehaviour {
+	void Start() {
+		if (GetComponent<VRTK_InteractableObject> () == null) {
+			return;
+		}
+
+		GetComponent<VRTK_InteractableObject> ().InteractableObjectGrabbed += new InteractableObjectEventHandler (ObjectGrabbed);
+	}
+
+	private void ObjectGrabbed(object sender, InteractableObjectEventArgs e) {
+		Instantiate ((VRTK_InteractableObject)sender);
+	}
+}
