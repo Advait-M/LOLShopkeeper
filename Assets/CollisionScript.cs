6f8e9d9157193971ea540ff07b07757cd3e106a2
Lord and Master of All Things Big and Small <amphy.kit@gmail.com>
2017-11-09 10:53:57 -0800

    added naive collision script

---
+++ Assets/CollisionScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		print (other.GetComponent<Material>());
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		print (other.GetComponent<Material>());
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
